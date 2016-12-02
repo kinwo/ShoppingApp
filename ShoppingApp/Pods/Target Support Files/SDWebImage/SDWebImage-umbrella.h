50f2fac0415fad471dff9c41fa2a9804a19dc8a6
Henry Chan <henry@kinwo.net>
2016-12-03 10:51:02 +1100

    [Pod] Make SDWebImage version specific, add gitignore

--- ShoppingApp/Pods/Target Support Files/SDWebImage/SDWebImage-umbrella.h
+++ ShoppingApp/Pods/Target Support Files/SDWebImage/SDWebImage-umbrella.h
@@ -1,4 +1,14 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
 
 #import "NSData+ImageContentType.h"
 #import "SDImageCache.h"
@@ -15,7 +25,6 @@
 #import "UIImageView+HighlightedWebCache.h"
 #import "UIImageView+WebCache.h"
 #import "UIView+WebCacheOperation.h"
-#import "UIImage+WebP.h"
 
 FOUNDATION_EXPORT double SDWebImageVersionNumber;
 FOUNDATION_EXPORT const unsigned char SDWebImageVersionString[];
